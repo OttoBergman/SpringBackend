de3aacc0a4726503df91ff091c876c87b8be277b
oerlex <alex.sp@posteo.de>
2016-01-07 11:29:51 +0100

    added member stuff

---
+++ src/main/java/com/vis/domain/Event.java
@@ -0,0 +1,7 @@
+package com.vis.domain;
+
+/**
+ * Created by oerlex on 2016-01-07.
+ */
+public class Event {
+}
--- src/main/java/com/vis/domain/Member.java
+++ src/main/java/com/vis/domain/Member.java
@@ -1,6 +1,7 @@
 package com.vis.domain;
 
 import javax.persistence.*;
+import java.util.Date;
 
 /**
  * Created by otto on 2016-01-07.
@@ -11,9 +12,50 @@ import javax.persistence.*;
 public class Member {
 
     @Id
-    @Column(name = "MEMBER_ID")
+    @Column(name = "VIS number")
     private Long id;
 
+    @Column(name = "First name")
+    private String name;
+
+    @Column(name = "Last name")
+    private String familyName;
+
+    @Column(name = "Gender")
+    private String sex;
+
+    @Column(name = "Email")
+    private String email;
+
+    @Column(name = "Birthday")
+    private Date birthday;
+
+    @Column(name = "Address")
+    private String address;
+
+    @Column(name = "Phone")
+    private String phoneNumber;
+
+    @Column(name = "Country of origin")
+    private String origiin;
+
+    @Column(name = "Student type")
+    private String studentType;
+
+    @Column(name = "Accept data collection")
+    private boolean acceptsDataCollection;
+
+    @Column(name = "Can be shared")
+    private boolean acceptsSharing;
+
+    @Column(name = "Length of membership")
+    private String lengthMembership;
+
+    @Column(name = "Diet Restriction")
+    private String dietRestriction;
+
+    @Column(name = "Date added")
+    private String dateAdded;
 
     public Member() {
     }
